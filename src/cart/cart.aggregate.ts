6f2e81cd45f8de191166ef893212821cb8a1f23c
benweizhu <bwzhu@thoughtworks.com>
2021-07-20 16:19:20 +0800

    add event version in getCurrentStateOfCart

--- src/cart/cart.aggregate.ts
+++ src/cart/cart.aggregate.ts
@@ -1,12 +1,19 @@
-import { eventStorage } from "./cart.event";
+import { Event, eventStorage } from "./cart.event";
 import { handlerReducer } from "./cart.handler";
 import { Cart } from "./cart.model";
+
+function getLastEventVersion(events: Array<Event>) {
+    if (events && events.length > 0) {
+        return events[events.length - 1].eventVersion;
+    }
+    return 0;
+}
 
 export function getCurrentStateOfCart() {
     const events = eventStorage.query();
     const currentStateOfCart = events.reduce(handlerReducer, new Cart('7643234567123'));
     return {
         cart: currentStateOfCart,
-        lastEventVersion: 1
+        lastEventVersion: getLastEventVersion(events)
     };
 }
